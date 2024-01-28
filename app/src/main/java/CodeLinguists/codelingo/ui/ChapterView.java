761eca8cd41e8c350c8853738d0b567628265235
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-01-27 18:03:13 -0600

    Initial activities created

---
+++ app/src/main/java/CodeLinguists/codelingo/ui/ChapterView.java
@@ -0,0 +1,16 @@
+package CodeLinguists.codelingo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import CodeLinguists.codelingo.R;
+
+public class ChapterView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_view);
+    }
+}
