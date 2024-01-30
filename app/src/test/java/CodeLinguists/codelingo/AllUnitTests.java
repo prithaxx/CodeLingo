4cc246ccf0f85e27eb269e3fcafbe30909e25997
Josh <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-01-30 16:53:30 -0600

    Added Services.java tests

---
+++ app/src/test/java/CodeLinguists/codelingo/AllUnitTests.java
@@ -0,0 +1,15 @@
+package CodeLinguists.codelingo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import CodeLinguists.codelingo.application.Services;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    Services.class
+})
+
+
+public class AllUnitTests {
+}
