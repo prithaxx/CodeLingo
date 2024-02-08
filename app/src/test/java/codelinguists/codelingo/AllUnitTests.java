acf7bc0b22742b8fdfb7350faa21a40b848dca54
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-02-08 11:24:36 -0600

    Updated AllUnitTests

--- app/src/test/java/codelinguists/codelingo/AllUnitTests.java
+++ app/src/test/java/codelinguists/codelingo/AllUnitTests.java
@@ -4,10 +4,18 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import codelinguists.codelingo.application.ServicesTest;
+import codelinguists.codelingo.logic.AccountHandlerTest;
+import codelinguists.codelingo.logic.ExceptionsTest;
+import codelinguists.codelingo.logic.QuizHandlerTest;
+import codelinguists.codelingo.logic.SessionManagerTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    ServicesTest.class
+    ServicesTest.class,
+    AccountHandlerTest.class,
+    ExceptionsTest.class,
+    QuizHandlerTest.class,
+    SessionManagerTest.class,
 })
 public class AllUnitTests {
 }
