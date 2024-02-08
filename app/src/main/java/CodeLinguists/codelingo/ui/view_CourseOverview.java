53ccbb92de348c2cd115255f21d7ae458a449456
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-02-08 07:36:38 -0600

    Basic chapter selection implemented

--- app/src/main/java/CodeLinguists/codelingo/ui/view_CourseOverview.java
+++ app/src/main/java/CodeLinguists/codelingo/ui/view_CourseOverview.java
@@ -25,107 +25,25 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
 import CodeLinguists.codelingo.R;
+import CodeLinguists.codelingo.logic.ISessionManager;
+import CodeLinguists.codelingo.logic.SessionManager;
+import CodeLinguists.codelingo.persistence.ISessionData;
 
-	public class view_CourseOverview extends Activity {
-
-	
-	private View _bg__surface_pro_8___1_ek2;
-	private View window;
-	private View navbar;
-	private ImageView codelingo_logo_1;
-	private TextView codelingo;
-	private ImageView outer_box;
-	private View rectangle_1;
-	private View innerrect1;
-	private View innerrect1_ek1;
-	private View innerrect1_ek2;
-	private View innerrect1_ek3;
-	private View rectangle_2;
-	private ImageView design2;
-	private View rectangle_3;
-	private ImageView design3;
-	private View rectangle_4;
-	private ImageView design4;
-	private TextView placeholder_course;
-	private ImageView line_1;
-	private ImageView untitled_design__7__1;
-	private TextView ex_1;
-	private TextView ex_4;
-	private TextView ex_3;
-	private TextView ex_2;
-	private ImageView star_1;
-	private ImageView star_2;
-	private ImageView star_5;
-	private ImageView star_4;
-	private ImageView star_3;
-	private ImageView star_16;
-	private ImageView star_17;
-	private ImageView star_11;
-	private ImageView star_12;
-	private ImageView star_14;
-	private ImageView star_15;
-	private ImageView star_6;
-	private ImageView star_7;
-	private ImageView star_10;
-	private ImageView untitled_design__8__1;
-	private TextView loren_ipsum;
-	private View _bg__align_justify_ek1;
-	private ImageView vector;
-
+	public class view_CourseOverview extends AppCompatActivity {
+	private ISessionManager sessionManager;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_course_overview);
 
-		
-		_bg__surface_pro_8___1_ek2 = (View) findViewById(R.id._bg__surface_pro_8___1_ek2);
-		window = (View) findViewById(R.id.window);
-		navbar = (View) findViewById(R.id.navbar);
-		codelingo_logo_1 = (ImageView) findViewById(R.id.codelingo_logo_1);
-		codelingo = (TextView) findViewById(R.id.codelingo);
-		outer_box = (ImageView) findViewById(R.id.outer_box);
-		rectangle_1 = (View) findViewById(R.id.rectangle_1);
-		innerrect1 = (View) findViewById(R.id.innerrect1);
-		innerrect1_ek1 = (View) findViewById(R.id.innerrect1_ek1);
-		innerrect1_ek2 = (View) findViewById(R.id.innerrect1_ek2);
-		innerrect1_ek3 = (View) findViewById(R.id.innerrect1_ek3);
-		rectangle_2 = (View) findViewById(R.id.rectangle_2);
-		design2 = (ImageView) findViewById(R.id.design2);
-		rectangle_3 = (View) findViewById(R.id.rectangle_3);
-		design3 = (ImageView) findViewById(R.id.design3);
-		rectangle_4 = (View) findViewById(R.id.rectangle_4);
-		design4 = (ImageView) findViewById(R.id.design4);
-		placeholder_course = (TextView) findViewById(R.id.placeholder_course);
-		line_1 = (ImageView) findViewById(R.id.line_1);
-		untitled_design__7__1 = (ImageView) findViewById(R.id.untitled_design__7__1);
-		ex_1 = (TextView) findViewById(R.id.ex_1);
-		ex_4 = (TextView) findViewById(R.id.ex_4);
-		ex_3 = (TextView) findViewById(R.id.ex_3);
-		ex_2 = (TextView) findViewById(R.id.ex_2);
-		star_1 = (ImageView) findViewById(R.id.star_1);
-		star_2 = (ImageView) findViewById(R.id.star_2);
-		star_5 = (ImageView) findViewById(R.id.star_5);
-		star_4 = (ImageView) findViewById(R.id.star_4);
-		star_3 = (ImageView) findViewById(R.id.star_3);
-		star_16 = (ImageView) findViewById(R.id.star_16);
-		star_17 = (ImageView) findViewById(R.id.star_17);
-		star_11 = (ImageView) findViewById(R.id.star_11);
-		star_12 = (ImageView) findViewById(R.id.star_12);
-		star_14 = (ImageView) findViewById(R.id.star_14);
-		star_15 = (ImageView) findViewById(R.id.star_15);
-		star_6 = (ImageView) findViewById(R.id.star_6);
-		star_7 = (ImageView) findViewById(R.id.star_7);
-		star_10 = (ImageView) findViewById(R.id.star_10);
-		untitled_design__8__1 = (ImageView) findViewById(R.id.untitled_design__8__1);
-		loren_ipsum = (TextView) findViewById(R.id.loren_ipsum);
-//		_bg__align_justify_ek1 = (View) findViewById(R.id._bg__align_justify_ek1);
-		vector = (ImageView) findViewById(R.id.vector);
-	
-		
-		//custom code goes here
-	
+		this.sessionManager = SessionManager.newInstance();
+
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.replace(R.id.fragmentContainerView3, cont_CourseOverview.newInstance()).commit();
 	}
 }
 	
