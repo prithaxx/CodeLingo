8e1a3ba94b8e9161665b1d8f67b884e12f9b1dcb
Fyfe-c <111027432+Fyfe-c@users.noreply.github.com>
2024-03-02 18:40:55 -0600

    set up db
    set up db

---
+++ app/src/main/java/CodeLinguists/codelingo/application/Main.java
@@ -0,0 +1,23 @@
+package CodeLinguists.codelingo.application;
+
+public class Main {
+    private static String dbName = "CodelingoDB";
+
+    public static void setDBPathName(final String name) {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dbName = name;
+    }
+
+    public static String getDBPathName() {
+        return dbName;
+    }
+
+}
