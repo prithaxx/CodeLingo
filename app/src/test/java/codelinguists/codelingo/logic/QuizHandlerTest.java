45be154e4efd838b4a1043b1699e369606d32cf1
18nghiado <doducnghia2002@gmail.com>
2024-03-07 01:06:06 -0600

    Added tests in QuizHandlerTest
    Some tests are failing prob because of QuizIterator

--- app/src/test/java/codelinguists/codelingo/logic/QuizHandlerTest.java
+++ app/src/test/java/codelinguists/codelingo/logic/QuizHandlerTest.java
@@ -9,14 +9,46 @@ import java.util.List;
 
 import CodeLinguists.codelingo.dso.QuestionType;
 import CodeLinguists.codelingo.dso.QuizObj;
+import CodeLinguists.codelingo.logic.IQuizIterator;
+import CodeLinguists.codelingo.logic.QuizHandler;
 import CodeLinguists.codelingo.logic.QuizIterator;
+import CodeLinguists.codelingo.logic.logic_exceptions.InputValidationException;
+import CodeLinguists.codelingo.persistence.stubs.ChapterDataStub;
+import CodeLinguists.codelingo.persistence.stubs.QuizDataStub;
 
 public class QuizHandlerTest {
     List<QuizObj> quizList;
+    QuizDataStub quizDataStub;
+    IQuizIterator quizIterator;
+    private QuizHandler quizHandler;
 
     @Before
     public void setUp() throws Exception {
+        quizDataStub = new QuizDataStub();
+        ChapterDataStub chapterDataStub = new ChapterDataStub();
         quizList = new ArrayList<>();
+        quizHandler = new QuizHandler(new QuizDataStub());
+    }
+
+    @Test
+    public void testGetQuiz(){
+        int courseId = 1;
+        int chapterId = 1;
+        quizIterator = quizHandler.getQuiz(courseId, chapterId);
+
+        assertNotNull(quizIterator);
+        assertTrue(quizIterator.hasNextQuestion());
+    }
+
+    @Test
+    public void testCheckQuizAnswer() throws InputValidationException {
+
+        int quizId = 3;
+        int chapterId = 1;
+        QuizObj quiz = quizDataStub.getQuizById(quizId, chapterId);
+
+        assertTrue(quizHandler.checkQuizAnswer(quiz, "Correct answer"));
+        assertFalse(quizHandler.checkQuizAnswer(quiz, "Incorrect Answer 1"));
     }
 
     @Test
