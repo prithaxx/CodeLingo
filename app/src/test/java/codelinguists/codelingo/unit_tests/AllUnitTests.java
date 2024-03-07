10df1e78c6c53c4412be0f5c2315b9365ac5516a
Josh <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-03-07 14:45:38 -0600

    Delete unused tests

--- app/src/test/java/codelinguists/codelingo/unit_tests/AllUnitTests.java
+++ app/src/test/java/codelinguists/codelingo/unit_tests/AllUnitTests.java
@@ -3,11 +3,6 @@ package codelinguists.codelingo.unit_tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import codelinguists.codelingo.unit_tests.application.ServicesTest;
-import codelinguists.codelingo.unit_tests.dso.AccountObjTest;
-import codelinguists.codelingo.unit_tests.dso.ChapterObjTest;
-import codelinguists.codelingo.unit_tests.dso.CourseObjTest;
-import codelinguists.codelingo.unit_tests.dso.QuizObjTest;
 import codelinguists.codelingo.unit_tests.logic.AccountHandlerTest;
 import codelinguists.codelingo.unit_tests.logic.CourseHandlerTest;
 import codelinguists.codelingo.unit_tests.logic.QuizHandlerTest;
