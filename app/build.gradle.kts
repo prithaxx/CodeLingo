b4b75bb3dfbc4a76484e5d1ac30b51b4a1b34246
Josh <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-03-23 04:29:53 -0500

    Added dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test:rules:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.4.0")
 }
