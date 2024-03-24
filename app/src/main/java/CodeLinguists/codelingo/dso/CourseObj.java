d42ab08902e2f7a64b00e5d72b1e0d3b8e19164e
Fyfe-c <111027432+Fyfe-c@users.noreply.github.com>
2024-03-23 21:57:59 -0500

    Update CourseObj.java
    change the object in case I can change the feature of the object. e.g. set()

--- app/src/main/java/CodeLinguists/codelingo/dso/CourseObj.java
+++ app/src/main/java/CodeLinguists/codelingo/dso/CourseObj.java
@@ -1,7 +1,41 @@
 package CodeLinguists.codelingo.dso;
+public class CourseObj {
+    private final int id;
+    private final String name;
+    private final String description;
+    private boolean isStarted;
+    private boolean isCompleted;
 
-public record CourseObj( int id,
-                        String name,
-                        String description,
-                        boolean isStarted,
-                        boolean isCompleted) {}
+    public CourseObj(int id, String name, String description, boolean isStarted, boolean isCompleted) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isStarted = isStarted;
+        this.isCompleted = isCompleted;
+    }
+
+    public boolean isStarted() {
+        return isStarted;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setStarted(boolean isStarted) {
+        this.isStarted = isStarted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+}
